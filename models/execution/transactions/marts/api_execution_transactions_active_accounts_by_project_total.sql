4d84ee47f034dd4d5b88b451bebfe4e6b92ee1fd
hdser <hugo.serodio@gnosis.io>
2025-10-15 14:26:42 +0200

    fix query filter

--- models/execution/transactions/marts/api_execution_transactions_active_accounts_by_project_total.sql
+++ models/execution/transactions/marts/api_execution_transactions_active_accounts_by_project_total.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='view', tags=['production','execution','transactions']) }}
 SELECT bucket AS label, value, change_pct
-FROM {{ ref('fct_execution_transactions_by_project_snapshots') }}
-WHERE label = 'ActiveAccounts' AND window = 'All'
+FROM {{ ref('fct_execution_transactions_by_project_snapshots') }} t
+WHERE t.label = 'ActiveAccounts' AND window = 'All'
 ORDER BY value DESC
