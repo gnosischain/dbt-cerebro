32f29055e219ea4b6eb44cf3b4853e329b8b5211
hdser <hugo.serodio@gnosis.io>
2025-10-15 14:12:32 +0200

    thread and fct to table

--- models/execution/transactions/marts/fct_execution_transactions_by_sector_daily.sql
+++ models/execution/transactions/marts/fct_execution_transactions_by_sector_daily.sql
@@ -1,17 +1,12 @@
 {{
   config(
     materialized='table',
-    tags=['production','execution','transactions'],
     engine='ReplacingMergeTree()',
-    partition_by=['toYYYYMM(date)'],
-    order_by=['date','sector'],
-    incremental_strategy='insert_overwrite',
-    on_schema_change='sync_all_columns',
-    unique_key=['date','sector']
+    order_by='(date, sector)',
+    tags=['production','execution','transactions']
   )
 }}
 
-{% set mf = apply_monthly_incremental_filter('date') %}
 
 SELECT
     date,
@@ -21,10 +16,5 @@ SELECT
     sum(gas_used_sum)                                 AS gas_used_sum,
     round(toFloat64(sum(fee_native_sum)), 6)          AS fee_native_sum
 FROM {{ ref('int_execution_transactions_by_project_daily') }}
-WHERE 1 = 1
-  {% if mf and mf | trim != '' %}
-    AND {{ mf }}
-  {% endif %}
-  AND date < today()
 GROUP BY
   date, sector
