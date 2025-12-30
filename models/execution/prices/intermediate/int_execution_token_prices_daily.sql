756a0cb831e185a3964c2adac23d9f58e426c40d
mlaegn <maximilian.laechelin@gnosis.io>
2025-12-30 12:00:14 +0000

    deduplication

--- models/execution/prices/intermediate/int_execution_token_prices_daily.sql
+++ models/execution/prices/intermediate/int_execution_token_prices_daily.sql
@@ -26,7 +26,7 @@ backedfi AS (
 wxdai_from_xdai AS (
     SELECT
         date,
-        'WXDAI',
+        'WXDAI' AS symbol,
         price
     FROM dune
     WHERE symbol = 'XDAI'
@@ -35,7 +35,7 @@ wxdai_from_xdai AS (
 agnosdai_from_sdai AS (
     SELECT
         date,
-        'AGNOSDAI',
+        'AGNOSDAI' AS symbol,
         price
     FROM dune
     WHERE symbol = 'SDAI'
@@ -50,15 +50,36 @@ usd_pegs AS (
         SELECT DISTINCT date FROM dune
     )
     ARRAY JOIN ['USDC','USDC.E','USDT'] AS symbol
+),
+
+all_prices AS (
+    SELECT date, symbol, price, 1 AS priority FROM dune
+    UNION ALL
+    SELECT date, symbol, price, 2 AS priority FROM backedfi
+    UNION ALL
+    SELECT date, symbol, price, 1 AS priority FROM wxdai_from_xdai
+    UNION ALL
+    SELECT date, symbol, price, 1 AS priority FROM agnosdai_from_sdai
+    UNION ALL
+    SELECT date, symbol, 1.0 AS price, 3 AS priority FROM usd_pegs
+),
+
+deduplicated AS (
+    SELECT
+        date,
+        symbol,
+        price
+    FROM (
+        SELECT
+            date,
+            symbol,
+            price,
+            row_number() OVER (PARTITION BY date, symbol ORDER BY priority) AS rn
+        FROM all_prices
+    )
+    WHERE rn = 1
 )
 
-SELECT date, symbol, price FROM dune
-UNION ALL
-SELECT date, symbol, price FROM backedfi
-UNION ALL
-SELECT * FROM wxdai_from_xdai
-UNION ALL
-SELECT * FROM agnosdai_from_sdai
-UNION ALL
-SELECT date, symbol, price FROM usd_pegs
+SELECT date, symbol, price
+FROM deduplicated
 ORDER BY date, symbol
