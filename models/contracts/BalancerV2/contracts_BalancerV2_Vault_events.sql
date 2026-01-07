c84bf1609066bbfd6af280ffd222f6ab74092375
mlaegn <maximilian.laechelin@gnosis.io>
2026-01-07 16:35:28 +0000

    add balancer v2 vault contract

---
+++ models/contracts/BalancerV2/contracts_BalancerV2_Vault_events.sql
@@ -0,0 +1,29 @@
+{{ 
+    config(
+        materialized            = 'incremental',
+        incremental_strategy    = 'delete+insert',
+        engine                  = 'ReplacingMergeTree()',
+        order_by                = '(block_timestamp, transaction_hash, log_index)',
+        unique_key              = '(block_timestamp, transaction_hash, log_index)',
+        partition_by            = 'toStartOfMonth(block_timestamp)',
+        settings                = { 
+                                    'allow_nullable_key': 1 
+                                },
+        pre_hook                = [
+                                    "SET allow_experimental_json_type = 1"
+                                ],
+        tags                    = ['production','contracts','balancerv2','events']
+    )
+}}
+
+
+{{ 
+    decode_logs(
+        source_table      = source('execution','logs'),
+        contract_address  = '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
+        output_json_type  = true,
+        incremental_column= 'block_timestamp',
+        start_blocktime   = '2022-12-12'  
+    )
+}}
+
