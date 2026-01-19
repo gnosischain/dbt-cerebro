7bcffc0ba0cbdf6663916b49997b3a2caed36c15
mlaegn <maximilian.laechelin@gnosis.io>
2026-01-19 16:49:24 +0800

    change logic for uniswap and swapr to transfer based

--- models/execution/pools/intermediate/int_execution_pools_balances_daily.sql
+++ models/execution/pools/intermediate/int_execution_pools_balances_daily.sql
@@ -1,4 +1,4 @@
-{{
+{{ 
     config(
         materialized='incremental',
         incremental_strategy='delete+insert',
@@ -7,7 +7,7 @@
         unique_key='(date, protocol, pool_address, token_address)',
         partition_by='toStartOfMonth(date)',
         settings={'allow_nullable_key': 1},
-        tags=['production', 'execution', 'pools', 'tvl', 'intermediate']
+        tags=['production', 'execution', 'pools', 'balances', 'intermediate']
     )
 }}
 
@@ -15,10 +15,9 @@
 {% set end_month   = var('end_month', none) %}
 
 WITH
-
 uniswap_v3_pools AS (
     SELECT DISTINCT
-        replaceAll(lower(decoded_params['pool']), '0x', '') AS pool_address,
+        lower(decoded_params['pool']) AS pool_address,
         lower(decoded_params['token0']) AS token0,
         lower(decoded_params['token1']) AS token1,
         'Uniswap V3' AS protocol
@@ -31,7 +30,7 @@ uniswap_v3_pools AS (
 
 swapr_v3_pools AS (
     SELECT DISTINCT
-        replaceAll(lower(decoded_params['pool']), '0x', '') AS pool_address,
+        lower(decoded_params['pool']) AS pool_address,
         lower(decoded_params['token0']) AS token0,
         lower(decoded_params['token1']) AS token1,
         'Swapr V3' AS protocol
@@ -42,10 +41,88 @@ swapr_v3_pools AS (
       AND decoded_params['token1'] IS NOT NULL
 ),
 
+pool_tokens AS (
+    SELECT
+        protocol,
+        pool_address,
+        token0 AS token_address
+    FROM uniswap_v3_pools
+
+    UNION ALL
+
+    SELECT
+        protocol,
+        pool_address,
+        token1 AS token_address
+    FROM uniswap_v3_pools
+
+    UNION ALL
+
+    SELECT
+        protocol,
+        pool_address,
+        token0 AS token_address
+    FROM swapr_v3_pools
+
+    UNION ALL
+
+    SELECT
+        protocol,
+        pool_address,
+        token1 AS token_address
+    FROM swapr_v3_pools
+),
+
+balances_daily AS (
+    SELECT
+        date,
+        lower(token_address) AS token_address,
+        lower(address) AS address,
+        balance_raw,
+        balance AS token_amount
+    FROM {{ ref('int_execution_tokens_balances_daily') }}
+    WHERE date < today()
+      {% if start_month and end_month %}
+        AND toStartOfMonth(date) >= toDate('{{ start_month }}')
+        AND toStartOfMonth(date) <= toDate('{{ end_month }}')
+      {% else %}
+        {{ apply_monthly_incremental_filter('date', 'date', 'true') }}
+      {% endif %}
+),
+
+transfer_based_balances AS (
+    SELECT
+        b.date AS date,
+        p.protocol AS protocol,
+        p.pool_address AS pool_address,
+        p.token_address AS token_address,
+        b.balance_raw AS token_amount_raw,
+        b.token_amount AS token_amount
+    FROM balances_daily b
+    INNER JOIN pool_tokens p
+        ON b.address = p.pool_address
+       AND b.token_address = p.token_address
+),
+
+balancer_v2_pool_registry AS (
+    SELECT
+        lower(decoded_params['poolId']) AS pool_id,
+        lower(decoded_params['poolAddress']) AS pool_address
+    FROM {{ ref('contracts_BalancerV2_Vault_events') }}
+    WHERE event_name = 'PoolRegistered'
+      AND decoded_params['poolId'] IS NOT NULL
+      AND decoded_params['poolAddress'] IS NOT NULL
+),
+
 balancer_v3_pool_tokens AS (
     SELECT
         replaceAll(lower(decoded_params['pool']), '0x', '') AS pool_address,
-        lower(JSONExtractString(token_val, '')) AS token_address,
+        lower(
+            concat(
+                '0x',
+                right(replaceAll(JSONExtractString(token_val, ''), '0x', ''), 40)
+            )
+        ) AS token_address,
         row_number() OVER (PARTITION BY lower(decoded_params['pool']) ORDER BY token_idx) - 1 AS token_index,
         'Balancer V3' AS protocol
     FROM {{ ref('contracts_BalancerV3_Vault_events') }}
@@ -55,81 +132,40 @@ balancer_v3_pool_tokens AS (
     WHERE event_name = 'PoolRegistered'
       AND decoded_params['pool'] IS NOT NULL
       AND decoded_params['tokenConfig'] IS NOT NULL
-      AND JSONExtractString(token_val, '') != '0x0000000000000000000000000000000000000000000000000000000000000000'
-),
-
-balancer_v3_pools AS (
-    SELECT
-        pool_address,
-        token_address,
-        token_index,
-        protocol
-    FROM balancer_v3_pool_tokens
-),
-
-uniswap_v3_deltas AS (
-    SELECT
-        e.pool_address,
-        e.block_timestamp,
-        e.transaction_hash,
-        e.log_index,
-        p.protocol,
-        CASE 
-            WHEN e.token_position = 'token0' THEN p.token0
-            WHEN e.token_position = 'token1' THEN p.token1
-        END AS token_address,
-        e.delta_amount_raw
-    FROM {{ ref('stg_pools__uniswap_v3_events') }} e
-    INNER JOIN uniswap_v3_pools p
-        ON e.pool_address = p.pool_address
-    WHERE e.delta_amount_raw IS NOT NULL
-),
-
-swapr_v3_deltas AS (
-    SELECT
-        e.pool_address,
-        e.block_timestamp,
-        e.transaction_hash,
-        e.log_index,
-        p.protocol,
-        CASE 
-            WHEN e.token_position = 'token0' THEN p.token0
-            WHEN e.token_position = 'token1' THEN p.token1
-        END AS token_address,
-        e.delta_amount_raw
-    FROM {{ ref('stg_pools__swapr_v3_events') }} e
-    INNER JOIN swapr_v3_pools p
-        ON e.pool_address = p.pool_address
-    WHERE e.delta_amount_raw IS NOT NULL
+      AND JSONExtractString(token_val, '') IS NOT NULL
+      AND replaceAll(JSONExtractString(token_val, ''), '0x', '') != '0000000000000000000000000000000000000000000000000000000000000000'
 ),
 
 balancer_v2_deltas AS (
     SELECT
-        e.pool_id AS pool_address,
         e.block_timestamp,
         e.transaction_hash,
         e.log_index,
         'Balancer V2' AS protocol,
-        e.token_address,
-        e.delta_amount_raw
+        lower(e.token_address) AS token_address,
+        e.delta_amount_raw,
+        coalesce(r.pool_address, e.pool_id) AS pool_address
     FROM {{ ref('stg_pools__balancer_v2_events') }} e
+    LEFT JOIN balancer_v2_pool_registry r
+        ON r.pool_id = e.pool_id
     WHERE e.delta_amount_raw IS NOT NULL
       AND e.token_address IS NOT NULL
+      AND e.pool_id IS NOT NULL
 ),
 
 balancer_v3_deltas_pool AS (
     SELECT
-        e.pool_address,
-        e.block_timestamp,
-        e.transaction_hash,
-        e.log_index,
-        p.protocol,
-        p.token_address,
-        e.delta_amount_raw
+        e.block_timestamp AS block_timestamp,
+        e.transaction_hash AS transaction_hash,
+        e.log_index AS log_index,
+        p.protocol AS protocol,
+        p.token_address AS token_address,
+        e.delta_amount_raw AS delta_amount_raw,
+        p.pool_address AS pool_address
     FROM {{ ref('stg_pools__balancer_v3_events') }} e
-    INNER JOIN balancer_v3_pools p
+    INNER JOIN balancer_v3_pool_tokens p
         ON e.pool_address = p.pool_address
-        AND toInt32OrNull(e.token_index) = p.token_index
+       AND toInt32OrNull(e.token_index) = p.token_index
     WHERE e.delta_amount_raw IS NOT NULL
       AND e.token_index IS NOT NULL
       AND e.pool_address IS NOT NULL
@@ -137,13 +173,13 @@ balancer_v3_deltas_pool AS (
 
 balancer_v3_deltas_swap AS (
     SELECT
-        e.pool_address,
-        e.block_timestamp,
-        e.transaction_hash,
-        e.log_index,
+        e.block_timestamp AS block_timestamp,
+        e.transaction_hash AS transaction_hash,
+        e.log_index AS log_index,
         'Balancer V3' AS protocol,
-        e.token_address,
-        e.delta_amount_raw
+        lower(e.token_address) AS token_address,
+        e.delta_amount_raw AS delta_amount_raw,
+        e.pool_address AS pool_address
     FROM {{ ref('stg_pools__balancer_v3_events') }} e
     WHERE e.event_type = 'Swap'
       AND e.delta_amount_raw IS NOT NULL
@@ -151,7 +187,7 @@ balancer_v3_deltas_swap AS (
       AND e.pool_address IS NOT NULL
 ),
 
-all_deltas AS (
+balancer_deltas AS (
     SELECT
         pool_address,
         toStartOfDay(block_timestamp) AS date,
@@ -160,10 +196,6 @@ all_deltas AS (
         token_address,
         delta_amount_raw
     FROM (
-        SELECT * FROM uniswap_v3_deltas
-        UNION ALL
-        SELECT * FROM swapr_v3_deltas
-        UNION ALL
         SELECT * FROM balancer_v2_deltas
         UNION ALL
         SELECT * FROM balancer_v3_deltas_pool
@@ -182,48 +214,51 @@ all_deltas AS (
       {% endif %}
 ),
 
-daily_deltas AS (
+balancer_daily_deltas AS (
     SELECT
         date,
         protocol,
         pool_address,
         token_address,
         sum(delta_amount_raw) AS daily_delta_raw
-    FROM all_deltas
+    FROM balancer_deltas
     GROUP BY date, protocol, pool_address, token_address
 ),
 
-
 {% if start_month and end_month %}
-prev_balances AS (
+balancer_prev_balances AS (
     SELECT
         protocol,
         pool_address,
         token_address,
         token_amount_raw AS balance_raw
     FROM {{ this }}
-    WHERE date = (
+    WHERE protocol IN ('Balancer V2', 'Balancer V3')
+      AND date = (
         SELECT max(date)
         FROM {{ this }}
         WHERE date < toDate('{{ start_month }}')
-    )
+          AND protocol IN ('Balancer V2', 'Balancer V3')
+      )
 ),
 {% elif is_incremental() %}
-prev_balances AS (
+balancer_prev_balances AS (
     SELECT
         protocol,
         pool_address,
         token_address,
         token_amount_raw AS balance_raw
     FROM {{ this }}
-    WHERE date = (
+    WHERE protocol IN ('Balancer V2', 'Balancer V3')
+      AND date = (
         SELECT max(date)
         FROM {{ this }}
-    )
+        WHERE protocol IN ('Balancer V2', 'Balancer V3')
+      )
 ),
 {% endif %}
 
-balances AS (
+balancer_balances AS (
     SELECT
         date,
         protocol,
@@ -238,13 +273,27 @@ balances AS (
             + coalesce(p.balance_raw, toInt256(0))
         {% endif %}
         AS balance_raw
-    FROM daily_deltas d
+    FROM balancer_daily_deltas d
     {% if is_incremental() %}
-    LEFT JOIN prev_balances p
+    LEFT JOIN balancer_prev_balances p
         ON d.protocol = p.protocol
         AND d.pool_address = p.pool_address
         AND d.token_address = p.token_address
     {% endif %}
+),
+
+balancer_balances_final AS (
+    SELECT
+        b.date AS date,
+        b.protocol AS protocol,
+        b.pool_address AS pool_address,
+        b.token_address AS token_address,
+        b.balance_raw AS token_amount_raw,
+        b.balance_raw / POWER(10, COALESCE(t.decimals, 18)) AS token_amount
+    FROM balancer_balances b
+    LEFT JOIN {{ ref('tokens_whitelist') }} t
+        ON lower(t.address) = b.token_address
+    WHERE b.balance_raw != 0
 )
 
 SELECT
@@ -252,10 +301,17 @@ SELECT
     protocol,
     pool_address,
     token_address,
-    balance_raw AS token_amount_raw,
-    balance_raw / POWER(10, COALESCE(t.decimals, 18)) AS token_amount
-FROM balances b
-LEFT JOIN {{ ref('tokens_whitelist') }} t
-    ON lower(t.address) = b.token_address
-WHERE balance_raw != 0
-ORDER BY date, protocol, pool_address, token_address
+    token_amount_raw,
+    token_amount
+FROM transfer_based_balances
+
+UNION ALL
+
+SELECT
+    date,
+    protocol,
+    pool_address,
+    token_address,
+    token_amount_raw,
+    token_amount
+FROM balancer_balances_final
