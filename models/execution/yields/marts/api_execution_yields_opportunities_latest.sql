90867cd9c8cfdf9f3d0ca637fb4558fde46c94ea
mlaegn <maximilian.laechelin@gnosis.io>
2026-01-29 12:05:37 +0800

    add yield overview models

---
+++ models/execution/yields/marts/api_execution_yields_opportunities_latest.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized='view',
+        tags=['production', 'execution', 'yields', 'api:yields_overview', 'granularity:latest']
+    )
+}}
+
+SELECT
+    type,
+    name,
+    yield_pct,
+    borrow_apy,
+    tvl,
+    protocol
+FROM {{ ref('fct_execution_yields_opportunities_latest') }}
+ORDER BY yield_pct DESC
