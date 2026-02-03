893c4d99b1b8aa62193d8cbe6b59a44e3f1c9fec
mlaegn <maximilian.laechelin@gnosis.io>
2026-02-03 17:49:50 +0800

    remove grouping by address

--- models/execution/tokens/marts/api_execution_tokens_supply_latest_by_token.sql
+++ models/execution/tokens/marts/api_execution_tokens_supply_latest_by_token.sql
@@ -10,6 +10,6 @@ SELECT
   argMax(supply, date) AS value
 FROM {{ ref('fct_execution_tokens_metrics_daily') }}
 WHERE date < today()
-GROUP BY token_address, symbol
+GROUP BY symbol
 ORDER BY token
 
