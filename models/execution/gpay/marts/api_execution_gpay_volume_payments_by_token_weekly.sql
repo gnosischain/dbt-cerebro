f49ae99064eca4e64ceda15f0c38aea5a18b318f
mlaegn <maximilian.laechelin@gnosis.io>
2026-02-11 16:03:31 +0800

    add fct

--- models/execution/gpay/marts/api_execution_gpay_volume_payments_by_token_weekly.sql
+++ models/execution/gpay/marts/api_execution_gpay_volume_payments_by_token_weekly.sql
@@ -6,12 +6,8 @@
 }}
 
 SELECT
-    toStartOfWeek(date, 1) AS week,
-    symbol                 AS token,
-    sum(amount)            AS volume,
-    sum(amount_usd)        AS volume_usd,
-    sum(payment_count)     AS payments
-FROM {{ ref('int_execution_gpay_payments_daily') }}
-WHERE date < today()
-GROUP BY week, token
-ORDER BY week, token
+    week   AS date,
+    token  AS label,
+    volume_usd AS value
+FROM {{ ref('fct_execution_gpay_volume_payments_by_token_weekly') }}
+ORDER BY date, label
