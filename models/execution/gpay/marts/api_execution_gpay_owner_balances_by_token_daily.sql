5b2e60209991013559274e678f595a47a2e24f59
mlaegn <maximilian.laechelin@gnosis.io>
2026-02-12 17:35:42 +0800

    add owner balances models

---
+++ models/execution/gpay/marts/api_execution_gpay_owner_balances_by_token_daily.sql
@@ -0,0 +1,14 @@
+{{
+  config(
+    materialized='view',
+    tags=['production','execution','gpay','tier1','api:gpay_owner_balances_by_token_daily','granularity:daily']
+  )
+}}
+
+SELECT
+    date,
+    symbol      AS label,
+    balance_usd AS value
+FROM {{ ref('fct_execution_gpay_owner_balances_by_token_daily') }}
+WHERE symbol IN ('EURe', 'GBPe', 'USDC.e')
+ORDER BY date, label
