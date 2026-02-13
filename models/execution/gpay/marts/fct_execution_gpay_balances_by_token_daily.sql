c12effa6dc80663fb3b5ebdd8a8d2066575cc7dc
mlaegn <maximilian.laechelin@gnosis.io>
2026-02-13 11:10:30 +0800

    add wallet balance cohorts, add filtering intermediate

--- models/execution/gpay/marts/fct_execution_gpay_balances_by_token_daily.sql
+++ models/execution/gpay/marts/fct_execution_gpay_balances_by_token_daily.sql
@@ -7,19 +7,11 @@
   )
 }}
 
-WITH gpay_wallets AS (
-    SELECT address
-    FROM {{ ref('stg_gpay__wallets') }}
-)
-
 SELECT
-    b.date,
-    b.symbol,
-    sum(b.balance)                        AS balance,
-    round(toFloat64(sum(b.balance_usd)), 2) AS balance_usd
-FROM {{ ref('int_execution_tokens_balances_daily') }} b
-WHERE b.address IN (SELECT address FROM gpay_wallets)
-  AND b.date >= '2023-06-01'
-  AND b.date < today()
-GROUP BY b.date, b.symbol
-ORDER BY b.date, b.symbol
+    date,
+    symbol,
+    sum(balance)                          AS balance,
+    round(toFloat64(sum(balance_usd)), 2) AS balance_usd
+FROM {{ ref('int_execution_gpay_balances_daily') }}
+GROUP BY date, symbol
+ORDER BY date, symbol
