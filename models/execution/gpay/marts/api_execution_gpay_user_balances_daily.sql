1729ec85044f156c66b5d65d640063d9bd7bf995
mlaegn <maximilian.laechelin@gnosis.io>
2026-02-26 16:52:51 +0800

    add unit toggle and token selector for user balances

--- models/execution/gpay/marts/api_execution_gpay_user_balances_daily.sql
+++ models/execution/gpay/marts/api_execution_gpay_user_balances_daily.sql
@@ -9,6 +9,8 @@ SELECT
     address AS wallet_address,
     date,
     symbol AS label,
-    round(toFloat64(balance_usd), 2) AS value
+    symbol AS token,
+    round(toFloat64(balance), 6)     AS value_native,
+    round(toFloat64(balance_usd), 2) AS value_usd
 FROM {{ ref('int_execution_gpay_balances_daily') }}
 ORDER BY date
