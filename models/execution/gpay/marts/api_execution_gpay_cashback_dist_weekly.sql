d863829eb50403860f0fed4c9f093345656a7a6a
mlaegn <maximilian.laechelin@gnosis.io>
2026-02-27 14:32:09 +0800

    remove quantiles usd for cashback distro

--- models/execution/gpay/marts/api_execution_gpay_cashback_dist_weekly.sql
+++ models/execution/gpay/marts/api_execution_gpay_cashback_dist_weekly.sql
@@ -7,8 +7,8 @@
 
 SELECT
     week AS date,
-    unit,
     q05, q10, q25, q50, q75, q90, q95,
     average
 FROM {{ ref('fct_execution_gpay_cashback_dist_weekly') }}
-ORDER BY date, unit
+WHERE unit = 'native'
+ORDER BY date
