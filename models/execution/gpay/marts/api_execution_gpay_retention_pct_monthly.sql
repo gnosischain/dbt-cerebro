b2154b3cb073e926391e61aecb9087e1b2c59845
mlaegn <maximilian.laechelin@gnosis.io>
2026-02-27 17:05:49 +0800

    correct retention model

--- models/execution/gpay/marts/api_execution_gpay_retention_pct_monthly.sql
+++ models/execution/gpay/marts/api_execution_gpay_retention_pct_monthly.sql
@@ -9,6 +9,7 @@ SELECT
     toString(activity_month) AS x,
     toString(cohort_month)   AS y,
     retention_pct            AS value_pct,
-    users                    AS value_abs
+    users                    AS value_abs,
+    amount_usd               AS value_usd
 FROM {{ ref('fct_execution_gpay_retention_monthly') }}
 ORDER BY y, x
