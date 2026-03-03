85b04c7dfc6892cc7d51ac97b85f4d02dd390c6c
mlaegn <maximilian.laechelin@gnosis.io>
2026-03-03 17:59:53 +0800

    edit opportunities table

--- models/execution/yields/marts/api_execution_yields_opportunities_latest.sql
+++ models/execution/yields/marts/api_execution_yields_opportunities_latest.sql
@@ -7,10 +7,13 @@
 
 SELECT
     type,
+    token,
     name,
     yield_pct,
+    yield_label,
     borrow_apy,
     tvl,
+    fees_7d,
     protocol
 FROM {{ ref('fct_execution_yields_opportunities_latest') }}
 ORDER BY yield_pct DESC
--- models/execution/yields/marts/fct_execution_yields_opportunities_latest.sql
+++ models/execution/yields/marts/fct_execution_yields_opportunities_latest.sql
@@ -10,10 +10,13 @@
 
 SELECT
     type,
+    token,
     name,
     yield_pct,
+    yield_label,
     borrow_apy,
     tvl,
+    fees_7d,
     protocol
 FROM (
     WITH
@@ -24,16 +27,32 @@ FROM (
         WHERE date < today()
     ),
     
+    lp_pool_fees_7d AS (
+        SELECT
+            f.pool,
+            sum(f.fees_usd_daily) AS fees_7d
+        FROM {{ ref('fct_execution_yields_pools_daily') }} f
+        CROSS JOIN pools_latest_date d
+        WHERE f.date > d.max_date - INTERVAL 7 DAY
+          AND f.date <= d.max_date
+          AND f.pool IS NOT NULL
+        GROUP BY f.pool
+    ),
+    
     lp_pools AS (
         SELECT
             'LP' AS type,
-            f.pool AS name,
+            f.token AS token,
+            replaceOne(f.pool, concat(' • ', f.protocol), '') AS name,
             f.fee_apr_7d AS yield_pct,
+            'APR' AS yield_label,
             NULL AS borrow_apy,
             f.tvl_usd AS tvl,
+            pf.fees_7d AS fees_7d,
             f.protocol AS protocol
         FROM {{ ref('fct_execution_yields_pools_daily') }} f
         CROSS JOIN pools_latest_date d
+        LEFT JOIN lp_pool_fees_7d pf ON pf.pool = f.pool
         WHERE f.date = d.max_date
           AND f.fee_apr_7d IS NOT NULL
           AND f.pool IS NOT NULL
@@ -42,10 +61,13 @@ FROM (
     lp_pools_dedup AS (
         SELECT
             type,
+            token,
             name,
             yield_pct,
+            yield_label,
             borrow_apy,
             tvl,
+            fees_7d,
             protocol
         FROM (
             SELECT
@@ -84,14 +106,17 @@ FROM (
     lending_markets AS (
         SELECT
             'Lending' AS type,
+            a.symbol AS token,
             a.symbol AS name,
             a.apy_daily AS yield_pct,
+            'APY' AS yield_label,
             a.borrow_apy_variable_daily AS borrow_apy,
             CASE
                 WHEN cs.total_supply_tokens IS NOT NULL AND cs.total_supply_tokens > 0
                 THEN cs.total_supply_tokens * coalesce(lp.price_usd, 0)
                 ELSE NULL
             END AS tvl,
+            NULL AS fees_7d,
             a.protocol AS protocol
         FROM {{ ref('int_execution_yields_aave_daily') }} a
         CROSS JOIN lending_latest_date d
@@ -105,12 +130,12 @@ FROM (
     )
     
     
-    SELECT type, name, yield_pct, borrow_apy, tvl, protocol
+    SELECT type, token, name, yield_pct, yield_label, borrow_apy, tvl, fees_7d, protocol
     FROM lp_pools_dedup
     
     UNION ALL
     
-    SELECT type, name, yield_pct, borrow_apy, tvl, protocol
+    SELECT type, token, name, yield_pct, yield_label, borrow_apy, tvl, fees_7d, protocol
     FROM lending_markets
 )
 ORDER BY yield_pct DESC
