e329c678d1d59afbe88c51f476218d764ea8c55b
mlaegn <maximilian.laechelin@gnosis.io>
2026-03-09 17:11:43 +0800

    add IL estimation, utilization rate, and data quality fixes

--- models/execution/yields/marts/api_execution_yields_opportunities_latest.sql
+++ models/execution/yields/marts/api_execution_yields_opportunities_latest.sql
@@ -14,6 +14,9 @@ SELECT
     borrow_apy,
     tvl,
     fees_7d,
+    il_apr_7d,
+    net_apr_7d,
+    utilization_rate,
     protocol
 FROM {{ ref('fct_execution_yields_opportunities_latest') }}
 ORDER BY yield_pct DESC
