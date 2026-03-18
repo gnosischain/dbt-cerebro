ffa678017a662188057ec19f884222cdb8fa4585
hdser <hugo.serodio@gnosis.io>
2026-03-18 17:21:23 +0100

    transaction models for active accounts

---
+++ models/execution/transactions/intermediate/int_execution_transactions_cumulative_daily.sql
@@ -0,0 +1,24 @@
+{{
+  config(
+    materialized='table',
+    engine='MergeTree()',
+    order_by='date',
+    tags=['production','execution','transactions']
+  )
+}}
+
+WITH address_first_seen AS (
+    SELECT
+        address_hash,
+        min(first_seen_date) AS first_seen_date
+    FROM {{ ref('int_execution_transactions_unique_addresses') }}
+    GROUP BY address_hash
+)
+
+SELECT
+    first_seen_date                                                     AS date,
+    toUInt64(count())                                                   AS new_accounts,
+    toUInt64(sum(count()) OVER (ORDER BY first_seen_date))              AS cumulative_accounts
+FROM address_first_seen
+GROUP BY first_seen_date
+ORDER BY first_seen_date
