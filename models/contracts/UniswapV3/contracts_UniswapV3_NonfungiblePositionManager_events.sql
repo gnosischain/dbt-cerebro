0636d768cf76c58ea6684e352a76cb88f674117b
mlaegn <maximilian.laechelin@gnosis.io>
2026-05-13 09:19:14 +0100

    add npm contract

---
+++ models/contracts/UniswapV3/contracts_UniswapV3_NonfungiblePositionManager_events.sql
@@ -0,0 +1,25 @@
+{{ 
+    config(
+        materialized            = 'incremental',
+        incremental_strategy    = ('append' if var('start_month', none) else 'delete+insert'),
+        engine                  = 'ReplacingMergeTree()',
+        order_by                = '(block_timestamp, transaction_hash, log_index)',
+        unique_key              = '(transaction_hash, log_index)',
+        partition_by            = 'toStartOfMonth(block_timestamp)',
+        settings                = { 
+                                    'allow_nullable_key': 1 
+                                },
+        tags                    = ['production','contracts','uniswapv3','events'],
+        pre_hook=["SET allow_experimental_json_type = 1"],
+        post_hook=["SET allow_experimental_json_type = 0"]
+    )
+}}
+{{ 
+    decode_logs(
+        source_table      = source('execution','logs'),
+        contract_address  = '0xae8fbe656a77519a7490054274910129c9244fa3',
+        output_json_type  = true,
+        incremental_column= 'block_timestamp',
+        start_blocktime   = '2022-04-22'  
+    )
+}}
